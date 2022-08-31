d4f860c727a072e7797c6d76c3cd6bc55e645685
Alex <al.repin.a@yandex.ru>
2022-08-31 10:41:51 +0300

    add Task019

---
+++ Task019/Program.cs
@@ -0,0 +1,18 @@
+﻿bool Palindrom(string number)
+{
+  int length = number.Length;
+  for (int i = 0; i < length-1-i; i++)
+  {
+    if (number[i] == number[length-1-i]) return true;
+  }
+  return false;
+}
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+Console.WriteLine(Palindrom(number));
+
+
+
+
+
